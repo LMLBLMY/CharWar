d6ca816d6e652f1324b12379be3c4b2c98189e8d
LMLBLMY <92493401+LMLBLMY@users.noreply.github.com>
2024-12-27 00:25:12 +0800

    Create CharDows.cpp(Ver241226)

---
+++ Support PROGs(其他配套程序)/Popover PROG(弹窗程序)/CharDows.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <stdio.h>
+#include <prime.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: program2.exe <int_value>\n");
+        return 1;
+    }
+
+    int Value = atoi(argv[1]); // 获取传入
+    char str[1024]{ 0 };
+
+    // 打印接收到的int值
+    printf("今天的起床代码: %d\n", Value);
+
+    switch (Value) {
+    case 1: {
+    }break;
+
+    default:
+
+        break;
+    }
+
+    initgraph(250, 150);
+    Prime_RECT();
+    TEXTOUT(L"程序已挂起", 50, 50);
+    //Sleep(1000);
+
+    system("pause");
+    return 1;
+}
